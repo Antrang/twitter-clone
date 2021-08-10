07aeebbd4074dee66cfa8b57c13d71689e30ebcf
Rohit Mahto <rohitmahto267@gmaill.com>
2021-08-11 01:13:10 +0530

    fixes

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.BACKEND_PORT || 5000;
+const path = require('path');
 //DB Connection
 require("./configs/database").connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'build')));
 //Auth Route
 const authRoute = require('./routes/Auth');
 
